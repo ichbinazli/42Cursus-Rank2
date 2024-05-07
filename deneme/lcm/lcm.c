19bb1bcc26ceab979ae91a13f49bf3804dfef849
Nazlı Anapa <114147817+ichbinazli@users.noreply.github.com>
2024-05-07 14:12:29 +0300

    Add files via upload

---
+++ deneme/lcm/lcm.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+unsigned int    lcm(unsigned int a, unsigned int b)
+{
+    unsigned int n;
+    if(a == 0 && b == 0)
+        return(0);
+    if(a > b)
+        n = a;
+    else
+        n = b;
+    while(1)
+    {
+        if(a % n == 0 & n % b == 0)
+            return(n);
+        ++n;
+    }
+}
